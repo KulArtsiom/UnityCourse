1cde12082c732b5ec3f1ef6cedd9f4d0a4fd7470
KulArtsiom <kulartsiom@gmail.com>
2019-12-17 11:04:34 +0300

    event system

---
+++ Assets/Scripts/EventsScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventsScript : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        float red = Random.Range(.0f, 1f);
+        float green = Random.Range(.0f, 1f);
+        float blue = Random.Range(.0f, 1f);
+        
+        Color color = new Color(red, green, blue);
+        gameObject.GetComponent<Renderer>().material.color = color;
+
+        Vector3 force = Camera.main.transform.forward  * 100;
+        Vector3 target = eventData.pointerPressRaycast.worldPosition;
+        
+        gameObject.GetComponent<Rigidbody>().AddForceAtPosition(force,target);
+    }
+}
