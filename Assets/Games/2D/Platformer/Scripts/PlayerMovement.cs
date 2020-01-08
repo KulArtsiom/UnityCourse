af89faf52d1db753969ba431d06b4c1b262d2b50
KulArtsiom <kulartsiom@gmail.com>
2020-01-08 16:42:28 +0300

    homework

--- Assets/Games/2D/Platformer/Scripts/PlayerMovement.cs
+++ Assets/Games/2D/Platformer/Scripts/PlayerMovement.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : CharacterMovement
 {
-    [SerializeField] private float maxSpeed = 10f;
     [SerializeField] private Transform graphics;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private Animator animator;
-    
+
     private Rigidbody2D rig;
+    private bool isRun;
+    private float maxSpeed;
     private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int JumpInt = Animator.StringToHash("Jump");
+    private static readonly int JumpPlayer = Animator.StringToHash("JumpPlayer");
+    private static readonly int Run = Animator.StringToHash("Run");
 
     private void Start()
     {
+        maxSpeed = 2f;
         rig = GetComponent<Rigidbody2D>();
         InputController.JumpAction += OnJumpAction;
     }
@@ -22,7 +27,6 @@ public class PlayerMovement : CharacterMovement
     private void OnDestroy()
     {
         InputController.JumpAction -= OnJumpAction;
-
     }
 
     private void FixedUpdate()
@@ -34,28 +38,55 @@ public class PlayerMovement : CharacterMovement
             rig.velocity = velocity;
             return;
         }
-        
+
         var direction = new Vector2(InputController.HorizontalAxis, 0f);
 
         if (!IsGrounded())
         {
             direction *= 0.5f;
         }
-        
+
         Move(direction);
-        
     }
 
     private void Update()
     {
         if (IsGrounded())
         {
-            animator.SetFloat(Speed,Mathf.Abs(rig.velocity.x));
+            animator.SetFloat(Speed, Mathf.Abs(rig.velocity.x));
+            if (isRun)
+            {
+                animator.SetFloat(Speed, Mathf.Abs(rig.velocity.x));
+                maxSpeed = 5f;
+            }
+            else
+            {
+                maxSpeed = 2f;
+            }
         }
         else
         {
-            animator.SetFloat(Speed,0);
+            animator.SetFloat(Speed, 0);
         }
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            if (!isRun)
+            {
+                isRun = true;
+                animator.SetBool(Run, true);
+                Debug.Log("Режим бега включен");
+            }
+            else
+            {
+                isRun = false;
+                animator.SetBool(Run, false);
+                Debug.Log("Режим бега выключен");
+            }
+        }
+
+     
+
 
         //Mathf.Abs - число по модулю
         if (Mathf.Abs(rig.velocity.x) < 0.01f)
@@ -63,7 +94,7 @@ public class PlayerMovement : CharacterMovement
             return;
         }
 
-        var angle = rig.velocity.x > 0f? 0f : 180f;
+        var angle = rig.velocity.x > 0f ? 0f : 180f;
         graphics.localEulerAngles = new Vector3(0f, angle, 0f);
     }
 
@@ -72,17 +103,15 @@ public class PlayerMovement : CharacterMovement
         Vector2 velocity = rig.velocity;
         velocity.x = direction.x * maxSpeed;
         rig.velocity = velocity;
-        
     }
 
     public override void Stop(float timer)
     {
-        
     }
 
     public override void Jump(float force)
     {
-        rig.AddForce(new Vector2(0,force), ForceMode2D.Impulse);
+        rig.AddForce(new Vector2(0, force), ForceMode2D.Impulse);
     }
 
     private void OnJumpAction(float force)
@@ -90,6 +119,7 @@ public class PlayerMovement : CharacterMovement
         if (IsGrounded() && !IsFreezing)
         {
             Jump(jumpForce * force);
+            animator.SetTrigger(JumpPlayer);
         }
     }
 
