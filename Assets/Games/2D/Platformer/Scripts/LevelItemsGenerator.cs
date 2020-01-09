f17b2d0f9989ae44fc7e129230dbe1355ddb4737
KulArtsiom <kulartsiom@gmail.com>
2020-01-09 17:44:01 +0300

    Update LevelItemsGenerator.cs

--- Assets/Games/2D/Platformer/Scripts/LevelItemsGenerator.cs
+++ Assets/Games/2D/Platformer/Scripts/LevelItemsGenerator.cs
@@ -5,45 +5,26 @@ using UnityEngine;
 public class LevelItemsGenerator : MonoBehaviour
 {
     
-    public int platformSize;
+    public int platformSize = 3;
     public GameObject firstPlatform;
     public GameObject midlePlatform;
     public GameObject lastPlatform;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
     
     [ContextMenu("Generate Platform")]
     private void GeneratePlatform()
     {
-        
         var first = Instantiate(firstPlatform);
         var pos = first.transform.position;
         pos.x += 4;
         first.transform.position = pos;
-        for (int i = 0; i <= platformSize; i++)
-        {
-            var midle = Instantiate(midlePlatform);
-            var posMidle = first.transform.position;
-            posMidle.x += 1.25f;
-            midle.transform.position = posMidle;
-            if (i == platformSize)
-            {
-                var last = Instantiate(lastPlatform);
-                var lastPos = midle.transform.position;
-                lastPos.x += 1.25f;
-                last.transform.position = lastPos;
-            }
-        }
+        var midle = Instantiate(midlePlatform);
+        var posMidle = first.transform.position;
+        posMidle.x += 1.25f;
+        midle.transform.position = posMidle;
+        var last = Instantiate(lastPlatform);
+        var lastPos = midle.transform.position;
+        lastPos.x += 1.25f;
+        last.transform.position = lastPos;
     }
     
     
