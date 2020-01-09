165e082acd120a1efdb9665161e006d9e277a0f0
KulArtsiom <kulartsiom@gmail.com>
2020-01-09 17:09:52 +0300

    homework - 2

---
+++ Assets/Games/2D/Platformer/Scripts/LevelItemsGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelItemsGenerator : MonoBehaviour
+{
+    
+    public int platformSize;
+    public GameObject firstPlatform;
+    public GameObject midlePlatform;
+    public GameObject lastPlatform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+    [ContextMenu("Generate Platform")]
+    private void GeneratePlatform()
+    {
+        
+        var first = Instantiate(firstPlatform);
+        var pos = first.transform.position;
+        pos.x += 4;
+        first.transform.position = pos;
+        for (int i = 0; i <= platformSize; i++)
+        {
+            var midle = Instantiate(midlePlatform);
+            var posMidle = first.transform.position;
+            posMidle.x += 1.25f;
+            midle.transform.position = posMidle;
+            if (i == platformSize)
+            {
+                var last = Instantiate(lastPlatform);
+                var lastPos = midle.transform.position;
+                lastPos.x += 1.25f;
+                last.transform.position = lastPos;
+            }
+        }
+    }
+    
+    
+}
