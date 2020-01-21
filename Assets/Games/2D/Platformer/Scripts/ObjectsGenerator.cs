ed6500b5828075846cad8100f6531306d6f199a7
KulArtsiom <kulartsiom@gmail.com>
2020-01-21 17:10:38 +0300

    lesson

---
+++ Assets/Games/2D/Platformer/Scripts/ObjectsGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ObjectsGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject[] prefabs;
+    [SerializeField] private bool isRandom;
+    [SerializeField] private float spawnTime = 1f;
+    private int current = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ObjectCreatorProccess());
+    }
+
+    private IEnumerator ObjectCreatorProccess()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+            if (isRandom)
+            {
+                current = Random.Range(0, prefabs.Length);
+            }
+
+            GameObject obj = Instantiate(prefabs[current]);
+            obj.transform.position = this.transform.position;
+            if (isRandom)
+            {
+                continue;
+            }
+
+            current++;
+            if (current >= prefabs.Length)
+            {
+                current = 0;
+            }
+        }
+        // ReSharper disable once IteratorNeverReturns
+    }
+}
--- Assets/Games/2D/Platformer/Scripts/Platform.cs
+++ Assets/Games/2D/Platformer/Scripts/Platform.cs
@@ -1,5 +1,59 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    [SerializeField] private bool isAnimated;
+    [SerializeField] private float animationDistance = 5f;
+    private float startX;
+
+    private void Start()
+    {
+        startX = transform.position.x;
+        if (isAnimated)
+        {
+            StartCoroutine(AnimatorProccess());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        bool isMovedObject = other.GetComponent<CharacterController>();
+        if (isMovedObject)
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.parent == transform)
+        {
+            other.transform.parent = null;
+        }
+    }
+
+    private IEnumerator AnimatorProccess()
+    {
+        var rightMovement = true;
+        var delta = 0f;
+
+        while (true)
+        {
+            delta += Time.deltaTime;
+            if (delta > 1f)
+            {
+                delta = 0;
+                rightMovement = !rightMovement;
+            }
+
+            Vector3 pos = transform.position;
+            var from = rightMovement ? startX : startX + animationDistance;
+            var to = rightMovement ? startX + animationDistance : startX;
+            pos.x = Mathf.Lerp(from, to, delta);
+            transform.position = pos;
+            yield return null;
+        }
+    }
 }
